a932372122efd518b9016d79f93a5fc18bb13fec
Andrew Mundy <andrew.mundy@ieee.org>
2016-01-25 12:20:59 +0000

    Further neaten Espresso calling code

--- experiments/espresso.py
+++ experiments/espresso.py
@@ -1,7 +1,7 @@
 import argparse
 import common
 from collections import defaultdict
-from rig.routing_table import RoutingTableEntry, table_is_subset_of
+from rig.routing_table import RoutingTableEntry
 from six import iteritems
 import subprocess
 import sys
@@ -130,7 +130,9 @@ def use_espresso_on_entire_table(table, provide_offset):
 
 
 def my_minimize(chip, table, whole_table, provide_offset):
-    sys.stdout.write("{:4d}\t".format(len(table)))
+    sys.stdout.write("({:3d}, {:3d})\t{:4d}\t".format(
+        chip[0], chip[1], len(table)))
+    sys.stdout.flush()
 
     if whole_table:
         new_table = use_espresso_on_entire_table(table, provide_offset)
@@ -138,7 +140,7 @@ def my_minimize(chip, table, whole_table, provide_offset):
         new_table = use_espresso(table, provide_offset)
 
     sys.stdout.write("\033[{}m{:4d}\033[39m\t{:.2f}%\n".format(
-        32 if table_is_subset_of(table, new_table) else 31,
+        32 if len(new_table) < 1024 else 31,
         len(new_table),
         100. * float(len(table) - len(new_table)) / len(table)
     ))
