a7a41c9197965402ac7308e654361511aa2bd87b
Andrew Mundy <andrew.mundy@ieee.org>
2016-01-27 19:20:19 +0000

    Neaten plots

--- experiments/plot_results.py
+++ experiments/plot_results.py
@@ -4,52 +4,59 @@ import numpy as np
 import seaborn as sns
 
 
-types = ("xyp", "xyzp", "hilbert")
-methods = ("esp_tables_no_offset", "esp_subtables_full", "oc", "pyoc")
+nets = ("gaussian", "centroid")
+
+methods = ("esp_tables_no_offset", "esp_subtables_full", "oc_spinnaker")
+method_titles = ("Espresso\nno DC set", "Espresso", "Ordered\nCovering")
 
 
 if __name__ == "__main__":
-    sns.set(style="whitegrid", font="Times New Roman")
+    sns.set(context="paper", style="whitegrid", font="Times New Roman")
 
     # Prepare the figures
-    fig, (ax0, ax1) = plt.subplots(2, sharex=True)
+    for model in nets:
+        fig, ax = plt.subplots(figsize=(3.5, 1.5))
 
-    # Label the x-axis now
-    xlabels = ["Original", "Remove Default Entries"]
-
-    for model, ax in (("gaussian", ax0), ("centroid", ax1)):
         ax.grid(False)
-
-        max_x = 2.5 + len(types)*len(methods) + .25
-        ax.set_xlim(-0.25, max_x)
-        ax.set_ylim(800, 1200)
-        ax.hlines(1024, -.25, max_x, linewidth=1, colors=[(.75, )*3])
-
-        with open("uncompressed/{}_12_12_xyp.bin".format(model), "rb") as f:
-            data = np.array(list(read_table_lengths(f).values()))
-            ax.violinplot(data, positions=[0], showextrema=False)
-
-        with open("compressed/remove_default_{}_12_12_xyp.bin".format(model), "rb") as f:
-            data = np.array(list(read_table_lengths(f).values()))
-            ax.violinplot(data, positions=[1], showextrema=False)
-
-        for i, t in enumerate(types):
-            for j, method in enumerate(methods):
-                try:
-                    fn = "compressed/{}_{}_12_12_{}.bin".format(method, model, t)
-                    pos = [2.5 + i*(len(methods) + .5) + j]
-                    with open(fn, "rb") as f:
-                        data = np.array(list(read_table_lengths(f).values()))
-                        ax.violinplot(data, positions=pos, showextrema=False)
-                except FileNotFoundError:
-                    pass
-
-    for ax in (ax0, ax1):
+        ax.hlines(1024, -.5, 8.5, linewidth=.5, colors=[(.5, )*3], zorder=0)
         ax.spines['top'].set_color('none')
         ax.spines['right'].set_color('none')
-        ax.spines['left'].set_position(('outward', 10))
+        ax.spines['bottom'].set_color('none')
+        ax.spines['left'].set_position(('outward', 5))
+        ax.set_ylabel("Table size")
 
-    ax0.spines['bottom'].set_color('none')
-    ax1.spines['bottom'].set_position(('outward', 10))
+        data = np.zeros((len(methods) + 2, 144))
 
-    plt.show()
+        # Read all the data
+        fn = "uncompressed/{}_12_12_xyp.bin".format(model)
+        with open(fn, "rb") as f:
+            data[0, :] = np.array(list(read_table_lengths(f).values()))
+
+        fn = "compressed/remove_default_{}_12_12_xyp.bin".format(model)
+        with open(fn, "rb") as f:
+            data[1, :] = np.array(list(read_table_lengths(f).values()))
+
+        for j, method in enumerate(methods):
+            fn = "compressed/{}_{}_12_12_xyp.bin".format(method, model)
+            with open(fn, "rb") as f:
+                data[j+2, :] = np.array(list(read_table_lengths(f).values()))
+
+        elems = ax.violinplot(data.T, np.arange(5)*2, showextrema=False)
+        for b in elems["bodies"]:
+            b.set_alpha(.75)
+            b.set_facecolor((.85, .85, .85))
+            b.set_edgecolor((0, 0, 0))
+            b.set_linewidth(1)
+
+        y_min = np.min(data)
+        y_max = np.max(data)
+        ax.set_ylim(y_min, y_max)
+        ax.set_yticks([y_min, 1024, y_max])
+
+        ax.set_xlim(-.5, 8.5)
+
+        ax.set_xticks(np.arange(5)*2)
+        ax.set_xticklabels(("Original", "With default\nrouting") + method_titles)
+
+        plt.tight_layout(pad=0)
+        plt.savefig("results_{}.pdf".format(model))
