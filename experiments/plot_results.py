de141dd114bdb086cc0f68a1392d9d221e815aa9
Andrew Mundy <andrew.mundy@ieee.org>
2016-01-29 11:45:31 +0000

    Join figures 4 and 5

--- experiments/plot_results.py
+++ experiments/plot_results.py
@@ -195,11 +195,14 @@ if __name__ == "__main__":
 if __name__ == "__main__":
     sns.set(context="paper", style="whitegrid", font="Times New Roman")
 
-    methods = ("esp_tables_no_offset", "esp_subtables_full", "mtrie")
-    method_titles = ("Espresso", "Order-\nExploiting\nEspresso", "m-Trie")
+    datasets = (("Original", "uncompressed/"),
+                ("With default\nrouting", "compressed/remove_default_"),
+                ("Espresso", "compressed/esp_tables_no_offset_"),
+                ("Order-\nexploiting\nEspresso", "compressed/esp_subtables_full_"),
+                ("m-Trie", "compressed/mtrie_"))
 
     # Prepare the figures
-    fig, axs = plt.subplots(1, 2, sharey=True, figsize=(3.5, 1.5))
+    fig, axs = plt.subplots(1, 2, sharey=True, figsize=(7, 1.75))
 
     axs[0].spines['left'].set_position(('outward', 5))
     axs[0].set_ylabel("Table size")
@@ -216,35 +219,40 @@ if __name__ == "__main__":
         ax.spines['right'].set_color('none')
         ax.spines['bottom'].set_color('none')
 
-        data = np.zeros((len(methods), 144))
+        data = np.zeros((len(datasets), 144))
 
         # Read all the data
-        for j, method in enumerate(methods):
-            fn = "compressed/{}_{}_12_12_xyp.bin".format(method, model)
+        for i, (_, fn) in enumerate(datasets):
+            fn = "{}{}_12_12_xyp.bin".format(fn, model)
             with open(fn, "rb") as f:
-                data[j, :] = np.array(list(read_table_lengths(f).values()))
+                data[i, :] = np.array(list(read_table_lengths(f).values()))
 
-        elems = ax.violinplot(data.T, np.arange(len(methods))*2, showextrema=False)
-        for b in elems["bodies"]:
-            b.set_alpha(.75)
+        xs = np.arange(len(datasets))*2
+        elems = ax.violinplot(data.T, xs, showextrema=False)
+        for b in elems["bodies"][:-2]:
+            b.set_alpha(.5)
             b.set_facecolor((.85, .85, .85))
             b.set_edgecolor((0, 0, 0))
             b.set_linewidth(1)
 
-        # The last element should be slightly highlighted to indicate that it is new
-        b.set_linewidth(1.5)
+        # The last 2 elements should be slightly highlighted
+        for b in elems["bodies"][-2:]:
+            b.set_alpha(.8)
+            b.set_facecolor((.85, .85, .85))
+            b.set_edgecolor((0, 0, 0))
+            b.set_linewidth(1)
 
         y_min = min(y_min, np.min(data))
         y_max = max(y_max, np.max(data))
 
-        ax.set_xlim(-.5, (len(methods) - 1)*2 + .5)
+        ax.set_xlim(-.5, max(xs) + .5)
 
-        ax.set_xticks(np.arange(len(methods) + 1)*2)
-        ax.set_xticklabels(method_titles)
-        ax.get_xticklabels()[-1].set_fontweight(1000)
+        titles = (t for t, _ in datasets)
+        ax.set_xticks(xs)
+        ax.set_xticklabels(titles)
 
     axs[0].set_ylim(y_min, y_max)
     axs[0].set_yticks([y_min, 1024, y_max])
 
-    plt.tight_layout(pad=0)
-    plt.savefig("results_with_mtrie.pdf")
+    plt.tight_layout(pad=0, w_pad=1.5)
+    plt.savefig("results_with_esp_dc_and_mtrie.pdf")
